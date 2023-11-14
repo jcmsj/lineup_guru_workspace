d19ce68d808de4d92265e407b718d05ef4649e9c
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2023-11-14 15:16:48 +0800

    Build script now deletes old apk

--- build_android.ps1
+++ build_android.ps1
@@ -2,7 +2,12 @@ function build_android($proj) {
     echo "Building $proj"
     cd $proj
     flutter build apk --no-tree-shake-icons
-    Move-Item -Path ".\build\app\outputs\flutter-apk\app-release.apk" -Destination "..\bin\$proj.apk"
+    # remove $proj.apk if it exists
+    $apk = "..\bin\$proj.apk"
+    if (Test-Path $apk) {
+        Remove-Item $apk
+    }
+    Move-Item -Path ".\build\app\outputs\flutter-apk\app-release.apk" -Destination $apk
     cd ..
 }
 
